f917b1da83ae3afcbcba41678e6b5e92c34c92be
SylvainFaure <s.faure@reply.it>
2018-07-05 17:00:20 +0200

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "../styles/App.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h1 className="app_title">React app yo</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: { 
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        } 
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader','sass-loader']
+          })
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+};
